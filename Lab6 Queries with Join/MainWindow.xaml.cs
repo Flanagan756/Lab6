eabfb29182e1d8951898133545244b00990beb25
Harry Flanagan <harryflanagan756@gmail.com>
2020-03-18 12:58:37 +0000

    Add project files.

---
+++ Lab6 Queries with Join/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab6_Queries_with_Join
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Ex1Button_Click(object sender, RoutedEventArgs e)
+        {
+       
+            var query = from c in db.Categories
+                        join p in db.Products on c.CategoryName equals p.Category.CategoryName
+                        orderby c.CategoryName
+                        select new {c.CategoryName,p.ProductName };
+
+            var result = query.ToList();
+
+            Ex1lbDisplay.ItemsSource = result;
+            EX1TblkCount.Text = result.Count.ToString();
+
+        }
+
+        private void Ex3Button_Click(object sender, RoutedEventArgs e)
+        {
+         
+            var query = from p in db.Products
+                        orderby p.Category.CategoryName, p.ProductName
+                        select new { Category = p.Category.CategoryName, Product = p.ProductName };
+
+            var result = query.ToList();
+
+            Ex2lbDisplay.ItemsSource = result;
+            EX2TblkCount.Text = result.Count.ToString();
+        }
+    }
+}
